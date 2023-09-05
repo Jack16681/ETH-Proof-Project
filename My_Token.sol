dd650b63fe211e6ab5f7be1226ec1092dec5cd72
Jack16681 <140252991+Jack16681@users.noreply.github.com>
2023-09-05 16:21:57 +0530

    Update My_Token.sol

--- My_Token.sol
+++ My_Token.sol
@@ -1,34 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract MyToken {
+contract My_Token {
     string public name = "OMKAR";
     string public symbol = "OK";
     uint8 public decimals = 8;
-    uint256 public totalSupply = 0; // Total supply with 18 decimal places
+    uint256 public totalSupply = 0; // Total supply with 8 decimal places
 
     mapping(address => uint256) public balanceOf;
 
     event Mint(address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
+    event Burn(address indexed from, address indexed to, uint256 value); // Added 'to' address attribute
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function mint(address to, uint256 value) public returns (bool) {
+    function mint(address to, uint256 value) public {
         totalSupply += value;
         balanceOf[to] += value;
-        emit Mint(to, value);
-        return true;
     }
 
-    function burn(address to, uint256 value) public returns (bool) {
-        require(balanceOf[msg.sender] >= value, "Insufficient balance for burning");
+    function burn(address from, uint256 value) public  {
+        require(balanceOf[from] >= value, "Insufficient balance for burning");
         
-        balanceOf[msg.sender] -= value;
+        balanceOf[from] -= value;
         totalSupply -= value;
-        emit Burn(msg.sender, value);
-        return true;
+        emit Burn(from, address(0), value); // Burning tokens by sending them to address(0)
     }
 }
